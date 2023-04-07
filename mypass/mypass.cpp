b9e026ff6bd242f26a9d4c1ebca0872bdd9ae086
Alan Yang <alan123piano@gmail.com>
2023-04-07 10:44:08 -0400

    minor

--- mypass/mypass.cpp
+++ mypass/mypass.cpp
@@ -2,26 +2,23 @@
 #include "llvm/IR/Function.h"
 #include "llvm/Support/raw_ostream.h"
 
-using namespace llvm;
-
 namespace
 {
-    struct Hello : public FunctionPass
+    struct Hello : public llvm::FunctionPass
     {
         static char ID;
         Hello() : FunctionPass(ID) {}
 
-        bool runOnFunction(Function &F) override
+        bool runOnFunction(llvm::Function &F) override
         {
-            errs() << "Hello: ";
-            errs().write_escaped(F.getName()) << '\n';
+            llvm::errs() << "Hello: ";
+            llvm::errs().write_escaped(F.getName()) << '\n';
             return false;
         }
-    }; // end of struct Hello
+    };
 }
-// end of anonymous namespace
 
 char Hello::ID = 0;
-static RegisterPass<Hello> X("hello", "Hello World Pass",
-                             false /* Only looks at CFG */,
-                             false /* Analysis Pass */);
+static llvm::RegisterPass<Hello> X("hello", "Hello World Pass",
+                                   false /* Only looks at CFG */,
+                                   false /* Analysis Pass */);
